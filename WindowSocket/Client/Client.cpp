e7a066cdff3bc0ce8c42c8a2f92cde84756023c4
이한성 <mast1207@naver.com>
2024-01-25 20:17:48 +0900

    간단한 채팅

--- WindowSocket/Client/Client.cpp
+++ WindowSocket/Client/Client.cpp
@@ -1,24 +1,36 @@
 ﻿#include <iostream>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+#include <string>
 #pragma comment(lib, "ws2_32.lib")
 
-int main()
-{
-	WSADATA wsaData;
-	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ClientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKET ClientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(10000);
-	inet_pton(AF_INET, "127.0.0.1", &(serverAddr.sin_addr));
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(10000);
+    inet_pton(AF_INET, "127.0.0.1", &(serverAddr.sin_addr));
 
-	connect(ClientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    connect(ClientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
 
-	const char* msg = "sdsd";
-	send(ClientSocket, msg, strlen(msg), 0);
-	closesocket(ClientSocket);
-	WSACleanup();
+    while (true) 
+    {
+        std::string msg;
+        std::cout << "Enter message: ";
+        std::getline(std::cin, msg);
+        send(ClientSocket, msg.c_str(), msg.size(), 0);
+
+        char recvMsg[1024]{};
+        int MsgByte = recv(ClientSocket, recvMsg, sizeof(recvMsg), 0);
+        recvMsg[MsgByte] = '\0';
+        std::cout << "Server response: " << recvMsg << std::endl;
+    }
+
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
 }
