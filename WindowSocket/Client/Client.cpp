0efe95bbab0eee565171d9f610a1672d9e5f6168
이한성 <mast1207@naver.com>
2024-01-25 16:46:44 +0900

    클라,서버 시작

---
+++ WindowSocket/Client/Client.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET ClientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(10000);
+	inet_pton(AF_INET, "127.0.0.1", &(serverAddr.sin_addr));
+
+	connect(ClientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	const char* msg = "sdsd";
+	send(ClientSocket, msg, strlen(msg), 0);
+	closesocket(ClientSocket);
+	WSACleanup();
+}
---
+++ WindowSocket/Server/Server.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <thread>
+#include <vector>
+#include <memory>
+#pragma comment(lib, "ws2_32.lib")
+
+std::vector<std::thread*> ThreadPool;
+int main()
+{
+	ThreadPool.reserve(100);
+	for (int i = 0; i < 10; i++)
+	{
+		std::thread* NewThread = new std::thread();
+		ThreadPool.push_back(NewThread);
+	}
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET ServSock = 0;
+	ServSock = socket(PF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN ServAddr;
+	ServAddr.sin_family = AF_INET;
+	ServAddr.sin_addr.s_addr = INADDR_ANY;
+	ServAddr.sin_port = htons(10000);
+
+	unsigned long a = htonl(000001);
+	bind(ServSock, (SOCKADDR*)&ServAddr, sizeof(ServAddr));
+	// 서버용 소켓을 만들었다. 이 소켓은 ipv4 형식이며 어떤 ip와도 통신할수있으며 포트번호는 10000이다.
+
+
+	listen(ServSock, 10);
+
+	SOCKET NewSock;
+	NewSock = accept(ServSock, nullptr, nullptr);
+
+	char RecvMsg[1000];
+	int resvBite = recv(NewSock, RecvMsg, sizeof(RecvMsg), 0);
+	RecvMsg[resvBite] = '\0';
+	std::cerr << RecvMsg;
+	closesocket(NewSock);
+	closesocket(ServSock);
+
+	WSACleanup();
+}
